7235272fc8b2cdfbf351d63e4bb34de2348b08ba
Danilo <dansantosalfa@gmail.com>
2023-10-29 09:58:03 -0300

    feat: Req2 concluido.

--- src/PaintShop/Room.cs
+++ src/PaintShop/Room.cs
@@ -3,7 +3,18 @@ namespace PaintShop;
 // 2 - Crie uma classe Room
 public class Room
 {
-    public double TotalPaintableArea;
+    public double TotalPaintableArea { get {
+            double area = 0;
+            foreach (var item in walls)
+            {
+                area += item.PaintableArea;
+            }
+            return area;    
+        }
+    }
+    public Wall[] walls { get; }
     public Room(Wall[] walls)
-    {}
+    {
+        this.walls = walls;
+    }
 }
