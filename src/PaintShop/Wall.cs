c3da57bd6f08a9e8220216939aaac8af18019b12
Danilo <dansantosalfa@gmail.com>
2023-10-29 09:36:17 -0300

    feat: req1 concluido.

--- src/PaintShop/Wall.cs
+++ src/PaintShop/Wall.cs
@@ -3,11 +3,32 @@
 // 1 - Crie uma classe Wall
 public class Wall
 {
-    public double Width;
-    public double Height;
-    public double ExcludedArea;
-    public double PaintableArea;
+    double _width;
+    public double Width {
+        get { return _width; }
+        set { if (value > 0) {
+            _width = value;
+            } else { _width = 1; }
+        }
+    }
+    double _height;
+    public double Height
+    {
+        get { return _height; }
+        set
+        {
+            if (value > 0)
+            {
+                _height = value;
+            }
+            else { _height = 1; }
+        }
+    }
+    public double ExcludedArea { get; set; }
+    public double PaintableArea { get { return Height * Width - ExcludedArea; } }
     public Wall(double width, double height)
     {
+        Width = width;
+        Height = height;
     }
 }
